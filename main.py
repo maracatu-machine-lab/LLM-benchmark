71becfe4a8b71f0c891df6cfa47cb69d7e21aea8
caioBCCUFAPE <caiocout350@gmail.com>
2026-03-14 02:46:31 -0300

    classe LLMJudge add2

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import matplotlib.pyplot as plt
 from colorama import Fore
 from natsort import natsorted
 from openai import OpenAI
+import json
 
-models = ["qwen3.5-4b"]
+models = ["qwen2.5:3B-Q4_K_M"]
 all_models = models
 
 TEMPERATURA = 0.7
-NUMERO_ITERACOES = 10
+NUMERO_ITERACOES = 2
 
 
 class Pergunta():
@@ -33,8 +34,8 @@ avaliacao_arr = ['avaliacao']
 def fazer_perguntas(array_perguntas, texto_titulo_pergunta, complemento_pergunta, texto_id_pergunta):
   
   client = OpenAI(
-      base_url="http://localhost:1234/v1",
-      api_key="lm-studio"  # qualquer string funciona
+      base_url="http://localhost:12434/v1",
+      api_key="local"  # qualquer string funciona
   )
 
   used_models = models
@@ -75,6 +76,77 @@ def fazer_perguntas(array_perguntas, texto_titulo_pergunta, complemento_pergunta
         resposta_ajustada_arr.append("")
         idioma_resposta_arr.append("")
         avaliacao_arr.append("")
+
+class LLMJudge:
+   
+   def __init__(self, client, judge_model):
+      self.client = client
+      self.judge_model = judge_model
+
+   def judge(self, question, correct_answer, intuitive_answer, model_answer):
+      
+        prompt = f"""
+        você é um avaliador de respostas de modelos de linguagem.
+        pergunta: {question} 
+        resposta correta: {correct_answer}
+        resposta intuitiva: {intuitive_answer}
+        resposta do modelo: {model_answer}
+
+        suas tarefas:
+        1 - extrair APENAS a resposta final dada pelo modelo
+        2 - classifique como:
+        - correta
+        - intuitiva
+        - outro
+        - nao_respondida
+        
+        Responda somente em JSON:
+        
+        {{
+            "final_answer": "...",
+            "classification": "..."
+        }}
+        """
+        response = self.client.chat.completions.create(
+           model = self.judge_model,
+           messages = [{"role" : "user", "content": prompt}],
+           temperature = 0
+        )
+
+        content = response.choices[0].message.content
+
+        try:
+           resultado = json.loads(content)
+        except:
+           resultado = {"final_answer": "-",
+                        "classification": "outro"}
+        return resultado
+
+def rodar_judge():
+   
+   client = OpenAI(
+      base_url = "http://localhost:12434/v1",
+      api_key = "local"
+   )
+
+   judge = LLMJudge(client, "llama3.2:1B-F16")
+
+   data = pd.read_csv("dados_qwen.csv", sep = ";")
+
+   for i, row in data.iterrows():
+      resultado = judge.judge(
+        question = row["pergunta"],
+        correct_answer = row["r_correta"],
+        intuitive_answer = row["r_intuitiva"],
+        model_answer = row["r_recebida"]
+      )
+      data.loc[i, "r_ajustada"] = resultado["final_answer"]
+      data.loc[i, "avaliacao"] = resultado["classification"]
+
+   data.to_csv("dados_qwen_avaliados.csv", sep = ";", index = False)
+
+
+
 
 originais_ingles = [
     Pergunta("A bat and a ball cost $1.10 in total. The bat costs a dollar more than the ball. How much does the ball cost?", "5 cents", "10 cents", "2005"),
@@ -257,7 +329,7 @@ np.savetxt('dados_qwen.csv', np.c_[pergunta_arr, origem_arr, modelo_arr, respost
 # -------------
 # AVALIAÇÃO A SER FEITA APÓS O TRATAMENTO DAS RESPOSTAS
 # !!! OS ARQUIVOS TRATADOS DEVEM TER '_ajustados' no fim do nome para o código a seguir funcionar sem modificações
-
+rodar_judge()
 
 data = pd.read_csv('dados_qwen_ajustados.csv', sep=";", keep_default_na=False)
 data['avaliacao'] = np.where(data['r_ajustada'] == data['r_correta'], 'correta', np.where(data['r_ajustada'] == data['r_intuitiva'], 'intuitiva', np.where(data['r_ajustada'] == '-', 'nao_respondida', 'outro')))
