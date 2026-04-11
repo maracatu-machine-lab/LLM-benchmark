0ea71d71c3698cc2b497798ae810c2d23c64ccb9
Thiago Cavalcanti <tcavalcanti.tc@gmail.com>
2026-04-11 01:39:58 -0300

    refac[gráficos]: criada classe para gráficos e removido código do main

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from colorama import Fore
 from openai import OpenAI
 from perguntas import configuracoes_perguntas
+from graficos import gerar_graficos
 
 models = ["qwen3.5-4b"]
 all_models = models
@@ -107,132 +107,4 @@ frame.to_csv('dados_tcc_todos_avaliados.csv', index=False, sep=";")
 
 # -------------
 # CRIAÇÃO DE GRÁFICOS
-
-# Idioma de resposta por modelo
-
-data = pd.read_csv('dados_tcc_todos_avaliados.csv', sep=";", keep_default_na=False)
-
-grafico = pd.crosstab(data['modelo'], data['idioma']).plot.bar(stacked=True, color=['red', 'gold'])
-grafico.set_ylabel('nº de respostas')
-grafico.legend(["inglês", "português"], title='idioma da resposta', bbox_to_anchor=(1.0, 1), loc='upper left')
-
-for rec in grafico.patches:
-    height = rec.get_height()
-    grafico.text(rec.get_x() + rec.get_width() / 2, rec.get_y() + height / 2, "{:.0f}".format(height), ha='center', va='bottom')
-
-plt.savefig('grafico_idioma_resposta_por_modelo.png', bbox_inches='tight')
-plt.show()
-
-# Idioma de resposta por tipo de pergunta por modelo
-
-data = pd.read_csv('dados_tcc_todos_avaliados.csv', sep=";", keep_default_na=False)
-
-for model in all_models:
-  data_modelo = data[data['modelo'] == model]
-
-  grafico = pd.crosstab(data_modelo['pergunta'].str.rpartition("_")[0], data_modelo['idioma']).plot.bar(stacked=True, color=['red', 'gold'])
-  grafico.set_title(model)
-  grafico.set_ylabel('nº de respostas')
-  grafico.set_xlabel('tipo de pergunta')
-  grafico.legend(["inglês", "português"], title='idioma da resposta', bbox_to_anchor=(1.0, 1), loc='upper left')
-
-  for rec in grafico.patches:
-      height = rec.get_height()
-      grafico.text(rec.get_x() + rec.get_width() / 2, rec.get_y() + height / 2, "{:.0f}".format(height), ha='center', va='bottom')
-
-  plt.savefig('grafico_idioma_resposta_por_tipo_pergunta_' + model +'.png', bbox_inches='tight')
-  plt.show()
-
-# Avaliação por modelo
-
-data = pd.read_csv('dados_tcc_todos_avaliados.csv', sep=";", keep_default_na=False)
-
-grafico = pd.crosstab(data['modelo'], data['avaliacao']).plot.bar(stacked=True)
-grafico.set_ylabel('nº de respostas')
-grafico.legend(title='avaliação recebida', bbox_to_anchor=(1.0, 1), loc='upper left')
-
-for rec in grafico.patches:
-    height = rec.get_height()
-    grafico.text(rec.get_x() + rec.get_width() / 2, rec.get_y() + height / 2, "{:.0f}".format(height), ha='center', va='bottom')
-
-plt.savefig('grafico_avaliacao_por_modelo.png', bbox_inches='tight')
-plt.show()
-
-# Avaliação por tipo de pergunta por modelo
-
-data = pd.read_csv('dados_tcc_todos_avaliados.csv', sep=";", keep_default_na=False)
-
-for model in all_models:
-  data_modelo = data[data['modelo'] == model]
-
-  grafico = pd.crosstab(data_modelo['pergunta'].str.rpartition("_")[0], data_modelo['avaliacao']).plot.bar(stacked=True)
-  grafico.set_title(model)
-  grafico.set_ylabel('nº de respostas')
-  grafico.set_xlabel('tipo de pergunta')
-
-  for rec in grafico.patches:
-      height = rec.get_height()
-      grafico.text(rec.get_x() + rec.get_width() / 2, rec.get_y() + height / 2, "{:.0f}".format(height), ha='center', va='bottom')
-
-  plt.savefig('grafico_resposta_por_tipo_pergunta_' + model +'.png', bbox_inches='tight')
-  plt.show()
-
-# Avaliação por tipo de pergunta
-
-def acertos_tipo_pergunta(tipo_pergunta, id_pergunta):
-  data = pd.read_csv('dados_tcc_todos_avaliados.csv', sep=";", keep_default_na=False)
-  data_perguntas = data[data['pergunta'].str.startswith(id_pergunta + '_')]
-
-  grafico = pd.crosstab(data_perguntas['modelo'], data_perguntas['avaliacao']).plot.bar(stacked=True)
-  grafico.set_title(tipo_pergunta)
-  grafico.set_ylabel('nº de respostas')
-  grafico.legend(title='avaliação recebida', bbox_to_anchor=(1.0, 1), loc='upper left')
-
-  for rec in grafico.patches:
-      height = rec.get_height()
-      grafico.text(rec.get_x() + rec.get_width() / 2, rec.get_y() + height / 2, "{:.0f}".format(height), ha='center', va='bottom')
-
-  plt.savefig('grafico_avaliacao_tipo_pergunta_' + id_pergunta.lower() + '.png', bbox_inches='tight')
-  plt.show()
-
-acertos_tipo_pergunta('original inglês', 'ORIG_ING')
-acertos_tipo_pergunta('original português', 'ORIG_POR')
-acertos_tipo_pergunta('original inglês com exemplo', 'ORIG_EX_ING')
-acertos_tipo_pergunta('original português com exemplo', 'ORIG_EX_POR')
-acertos_tipo_pergunta('nova inglês', 'NOVA_ING')
-acertos_tipo_pergunta('nova português', 'NOVA_POR')
-acertos_tipo_pergunta('nova inglês com exemplo', 'NOVA_EX_ING')
-acertos_tipo_pergunta('nova português com exemplo', 'NOVA_EX_POR')
-
-# Avaliação por pergunta por modelo
-
-def acertos_pergunta_modelo(tipo_pergunta, id_pergunta):
-  data = pd.read_csv('dados_tcc_todos_avaliados.csv', sep=";", keep_default_na=False)
-
-  for model in all_models:
-    data_modelo = data[data['modelo'] == model]
-    data_modelo = data_modelo[data_modelo['pergunta'].str.startswith(id_pergunta + '_')]
-
-    ids_perguntas = data_modelo['pergunta'].unique()
-    data_modelo['pergunta'] = pd.Categorical(data_modelo['pergunta'], ids_perguntas)
-
-    grafico = pd.crosstab(data_modelo['pergunta'], data_modelo['avaliacao']).sort_index().plot.bar(stacked=True)
-    grafico.set_title(model)
-    grafico.set_ylabel('número de respostas - ' + tipo_pergunta)
-    grafico.legend(title='avaliação recebida', bbox_to_anchor=(1.0, 1), loc='upper left')
-
-    for rec in grafico.patches:
-        height = rec.get_height()
-        grafico.text(rec.get_x() + rec.get_width() / 2, rec.get_y() + height / 2, "{:.0f}".format(height), ha='center', va='bottom')
-
-    plt.savefig('grafico_avaliacao_por_pergunta_' + id_pergunta.lower() + '_' + model +'.png', bbox_inches='tight')
-    plt.show()
-
-acertos_pergunta_modelo('originais inglês', 'ORIG_ING')
-acertos_pergunta_modelo('originais português', 'ORIG_POR')
-acertos_pergunta_modelo('originais inglês com exemplo', 'ORIG_EX_ING')
-acertos_pergunta_modelo('originais português com exemplo', 'ORIG_EX_POR')
-acertos_pergunta_modelo('novas inglês', 'NOVA_ING')
-acertos_pergunta_modelo('novas português', 'NOVA_POR')
-acertos_pergunta_modelo('novas inglês com exemplo', 'NOVA_EX_ING')
-acertos_pergunta_modelo('novas português com exemplo', 'NOVA_EX_POR')
+gerar_graficos(all_models)
