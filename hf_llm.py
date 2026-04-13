7f5d7b9fdf8f6b6d51047b9b72140366313f227e
Thiago Cavalcanti <tcavalcanti.tc@gmail.com>
2026-04-13 11:39:27 -0300

    Ajuste do nome do parâmetro do token

--- hf_llm.py
+++ hf_llm.py
@@ -7,18 +7,18 @@ class HuggingFaceLLM(BaseLLM):
     def __init__(self, model_name: str, hf_token: str = None):
         self.model_name = model_name
 
-        config = AutoConfig.from_pretrained(model_name)
+        config = AutoConfig.from_pretrained(model_name, token=hf_token)
         # Detecta tipo do modelo automaticamente e usa do cache ou baixa os pesos
         if config.is_encoder_decoder:
-            self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+            self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name, token=hf_token)
             task = "text2text-generation"
         else:
             # Pode acessar localmente como "./models/medgemma"
-            self.model = AutoModelForCausalLM.from_pretrained(model_name)
+            self.model = AutoModelForCausalLM.from_pretrained(model_name, token=hf_token)
             task = "text-generation"
 
 
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=hf_token)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name, token=hf_token)
         self.device = 0 if torch.cuda.is_available() else -1
         
         self.pipe = pipeline(
@@ -28,10 +28,11 @@ class HuggingFaceLLM(BaseLLM):
             device=self.device
         )
 
-    def generate(self, prompt: str, max_tokens: int = 600) -> str:
+    def generate(self, prompt: str, max_tokens: int = 200) -> str:
         config = GenerationConfig(
             max_new_tokens=max_tokens,
-            do_sample=False
+            temperature=0.7,
+            do_sample=True
         )
 
         output = self.pipe(prompt, generation_config=config)
